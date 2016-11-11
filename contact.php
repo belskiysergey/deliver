523bd5d58088bd2563406976e404de9db0a561ff
belskiysergey <belskiysergey@gmail.com>
2016-11-11 03:00:30 +0300

    Написал разметку и стили для страницы контактов

--- contact.php
+++ contact.php
@@ -4,43 +4,47 @@
         <meta charset="UTF-8">
         <title>Contact us</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/contact.css">
     </head>
     <body>
 
         <?php include("header.php"); ?>
 
         <section class="section-title">Contact us</section>
-        <section>
+        <section class="team">
             <div class="wrapp">
                 <h2>We would like to hear from you!</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
             </div>
         </section>
-        <section>
-            <div class="wrapp">
+        <section class="wrapper">
+            <div class="clearfix">
                 <div class="contact-form">
                     <h3>Contact form</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis, interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
 
                     <form action="">
-                        <label for="user_name">Name<span>You full name please.</span></label>
+                        <label for="user_name">Name</label>
+                            <div>You full name please.</div>
                         <input type="text" name="user_name">
 
-                        <label for="email">Email address <span>This will be the email I contact you with.</span></label>
+                        <label for="email">Email address</label>
+                            <div>This will be the email I contact you with.</div>
                         <input type="text" name="email">
 
-                        <label for="subject">Subject<span>Be creative if you like.</span></label>
+                        <label for="subject">Subject</label>
+                            <div>Be creative if you like.</div>
                         <input type="text" name="subject">
 
-                        <label for="message">Message<span>Please don't send me too long of a message ... jks!</span></label>
+                        <label for="message">Message</label>
+                            <div>Please don't send me too long of a message ... jks!</div>
                         <textarea name="message" id=""></textarea>
 
                         <button>Send Message</button>
                     </form>
                 </div>
                 <div class="company-info">
-                    <div class="our-location">
+                    <div class="info-block">
                         <h3>Our location</h3>
                         <p>We just moved to our new location down the block right beside Subway.</p>
                         <ul>
@@ -49,9 +53,9 @@
                             <li>S7R 1A3</li>
                         </ul>
                     </div>
-                    <div class="contact-information">
+                    <div class="info-block">
                         <h3>Contact information</h3>
-                        <div class="box">
+                        <div class="box clearfix">
                             <ul class="name-contact">
                                 <li>Email:</li>
                                 <li>Primary Phone:</li>
@@ -66,10 +70,10 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="office-hours">
+                    <div class="info-block">
                         <h3>Office hours</h3>
-                        <div class="box"></div>
-                            <ul class="days_of_the_week">
+                        <div class="box clearfix">
+                            <ul class="days-of-the-week">
                                 <li>Mounday - Friday</li>
                                 <li>Suturday - Sunday</li>
                                 <li>Holiday</li>
@@ -80,18 +84,20 @@
                                 <li>Closed</li>
                             </ul>
                         </div>
-                        <span>Feel free to email or call us after hours.</span>
                     </div>
+                    <span>Feel free to email or call us after hours.</span>
                 </div>
             </div>
         </section>
-        <section>
-            <h2>Do you need a theme?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
-            <a href="#">Purchase</a>
+        <section class="block-question">
+            <div class="wrapp">
+                <h2>Do you need a theme?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+                <a href="#">Purchase</a>
+            </div>
         </section>
 
-        <?php /*include("footer.php");*/ ?>
+        <?php include("footer.php"); ?>
 
     </body>
 </html>
