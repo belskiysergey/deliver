3bd978990ae4ab3e313624118d8d79faaac245e0
belskiysergey <belskiysergey@gmail.com>
2016-11-16 23:14:49 +0300

    Готова разметка и стили страницы archive

---
+++ archive.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Archive</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/archive.css">
+    </head>
+    <body>
+
+        <?php include("header.php"); ?>
+
+        <section class="section-title">Archive</section>
+        <section class="wrapper">
+            <div class="clearfix">
+                <div class="left-block">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis, interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+
+                    <div class="last-posts">
+                        <h3>Last 20 post</h3>
+                        <ul>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">New Premium WordPress Theme - Blaco</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Top Web Trends of 2013</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Video Post Example</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">How Important is Twitter</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Home Buiders New Planning Solution</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Slideshow Post Example</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Design Trends</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">New Photography By David</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Poetry in Web Design</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Deliver WordPress There</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Smashing Typography Examples</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Typical Day of a Designer</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Flat Web Designs</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Top Freebies of 2013</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Best PSD Freebies Recources</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Got To Keep Up To Date</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Time To Get Tech Savy</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Great UI Kits</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">What Makes a Good Web Design Standout</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Best Blog Post Types</a></li>
+                        </ul>
+                    </div>
+                    <div class="archives-by-month">
+                        <h3>Archives by month</h3>
+                        <ul>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">March (4)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">April (5)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">May (3)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">June (4)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">July (2)</a></li>
+                        </ul>
+                    </div>
+                    <div class="archives-by-category">
+                        <h3>Archives by category</h3>
+                        <ul>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">News (4)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Website Releases (23)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Events (7)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Marketing (5)</a></li>
+                            <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Web Development (2)</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="right-block">
+                    <div class="about-deliver-there">
+                        <h3>About Deliver There</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis, interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum est pretium.</p>
+                    </div>
+                    <div class="popular-posts">
+                        <h3>Popular Posts</h3>
+                        <ul>
+                            <li><a href="#">New Premium WordPress Theme - Blaco</a></li>
+                            <li><a href="#">Top Web Trends of 2013</a></li>
+                            <li><a href="#">Video Post Example</a></li>
+                            <li><a href="#">How Important is Twitter</a></li>
+                            <li><a href="#">Home Buiders New Planning Solution</a></li>
+                        </ul>
+                    </div>
+                    <!-- <div class="flickr-widget">
+                        <h3>Flickr Widget</h3>
+                        <div class="clearfix">
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                            <div><img src="@{images}/" alt=""></div>
+                        </div>
+                    </div>
+                    <div class="twitter-widget">
+                        <h3>Twitter Widget</h3>
+                        <div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </section>
+
+        <?php include("footer.php"); ?>
+
+    </body>
+</html>
