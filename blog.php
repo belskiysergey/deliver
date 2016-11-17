e1b5334c04d83bdde75cbda594aaa1ad4e6fb576
belskiysergey <belskiysergey@gmail.com>
2016-11-17 19:46:34 +0300

    Готова разметка и стили для страницы сервиса

---
+++ blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Our blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/blog.css">
+    </head>
+    <body>
+        <?php include("header.php"); ?>
+
+        <section class="section-title">Our blog</section>
+        <section>
+            <div class="left-block"></div>
+            <div class="right-block"></div>
+        </section>
+        <nav>
+            <ul>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+            </ul>
+        </nav>
+
+        <?php include("footer.php"); ?>
+    </body>
+</html>
---
+++ services.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Services</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/services.css">
+    </head>
+    <body>
+        <?php include("header.php"); ?>
+
+        <section class="section-title">Contact us</section>
+        <section class="team">
+            <div class="wrapp">
+                <h1>Ckeck out our awesome services</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+            </div>
+        </section>
+        <!--Описание сервиса-->
+        <section class="wrapper clearfix">
+            <div class="block-left">
+                <div class="bl-service clearfix">
+                    <div class="image icon-1"></div>
+                    <div class="service">
+                        <h3>Service title 1</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+
+                <div class="bl-service clearfix">
+                    <div class="image icon-2"></div>
+                    <div class="service">
+                        <h3>Service title 2</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+
+                <div class="bl-service clearfix">
+                    <div class="image icon-3"></div>
+                    <div class="service">
+                        <h3>Service title 3</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="block-right">
+                <div class="bl-service clearfix">
+                    <div class="image icon-4"></div>
+                    <div class="service">
+                        <h3>Service title 4</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+
+                <div class="bl-service clearfix">
+                    <div class="image icon-5"></div>
+                    <div class="service">
+                        <h3>Service title 5</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+
+                <div class="bl-service clearfix">
+                    <div class="image icon-6"></div>
+                    <div class="service">
+                        <h3>Service title 6</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lato justo ligula, interdum ut lobortis quis nulla sit amet rutest.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="our-work clearfix">
+            <div class="wrapp">
+                <h2>Pricing table example</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+                <div class="pricing clearfix">
+                    <div class="catalog">
+                        <div class="head">Basic</div>
+                        <ul>
+                            <li>$999<sup>99</sup></li>
+                            <li>Custom Design</li>
+                            <li>3 or Less Pages</li>
+                            <li>Basic SEO</li>
+                            <li>Contact Form</li>
+                            <li>Updates Allowed</li>
+                        </ul>
+                        <div class="bottom">
+                            <div>
+                                <a href="">Full details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="catalog">
+                        <div class="head">Standard</div>
+                        <ul>
+                            <li>$1799<sup>99</sup></li>
+                            <li>Choice of Templace</li>
+                            <li>5 to 10 Pages</li>
+                            <li>Basic SEO</li>
+                            <li>Contact Form</li>
+                            <li>Training Included</li>
+                        </ul>
+                        <div class="bottom">
+                            <div>
+                                <a href="">Full details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="catalog premium">
+                        <div class="head">Premium</div>
+                        <ul>
+                            <li>$3499<sup>99</sup></li>
+                            <li>Custom Design</li>
+                            <li>Unlimited Pages</li>
+                            <li>WordPress / CMS</li>
+                            <li>Advanced SEO</li>
+                            <li>Contact Form</li>
+                            <li>Training Included</li>
+                        </ul>
+                        <div class="bottom">
+                            <div>
+                                <a href="">Full details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="catalog">
+                        <div class="head">Custom</div>
+                        <ul>
+                            <li>$?</li>
+                            <li>Custom Design</li>
+                            <li>Unlimited Pages</li>
+                            <li>Advanced SEO</li>
+                            <li>WordPress / CMS</li>
+                            <li>Custom Functionality</li>
+                        </ul>
+                        <div class="bottom">
+                            <div>
+                                <a href="">Full details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <section class="information clearfix">
+            <div>
+                <h3>Good to know</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales purus vel nunc elementum rhoncus. Praesent neque metus, mollis sit amet convallis ut, tempus porta arcu. Phasellus accumsan facilisis viverra. Sed a velit sit amet nunc aliquam con sectetur.</p>
+            </div>
+            <div class="distance">
+                <h3>Cool Feature</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales purus vel nunc elementum rhoncus. Praesent neque metus, mollis sit amet convallis ut, tempus porta arcu. Phasellus accumsan facilisis viverra. Sed a velit sit amet nunc aliquam con sectetur.</p>
+            </div>
+            <div>
+                <h3>Something else to know</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales purus vel nunc elementum rhoncus. Praesent neque metus, mollis sit amet convallis ut, tempus porta arcu. Phasellus accumsan facilisis viverra. Sed a velit sit amet nunc aliquam con sectetur.</p>
+            </div>
+        </section>
+        <section class="block-question">
+            <div class="wrapp">
+                <h2>Do you need a there?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent justo ligula, interdum ut lobortis quis,interdum vitae metus. Proin fringilla metus non nulla cursus, sit amet rutrum ast pretium.</p>
+                <a href="javascript:void(0)">purchase</a>
+            </div>
+        </section>
+
+        <?php include("footer.php"); ?>
+
+    </body>
+</html>
